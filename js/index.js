3e0d857e16577870407fd1eea9f0e1d1b39919a3
Ivan Dobrolyubov <66012866+Khuro13@users.noreply.github.com>
2020-10-15 14:27:51 +0300

    timer

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]')
+};
+
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  setTimer() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      updateClockface(deltaTime);
+    }, 0);
+  }
+}
+
+const timer = new CountdownTimer(
+  "#timer-1",
+  new Date("December 17, 2020 12:00:00")
+);
+
+timer.setTimer();
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function updateClockface(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
+}
